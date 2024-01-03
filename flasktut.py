2405e60495f7220c727fd0e4b5102f7ba2c51aa4
mo pc <moalobaidly@gmail.com>
2024-01-03 00:21:35 +0000

    first attempt at a flask website

---
+++ flasktut.py
@@ -0,0 +1,17 @@
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return "Hello"
+@app.route("/<name>")
+def user(name):
+    return f"hello {name}"
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
+    
+
+if  __name__ == "__main__":
+    app.run()
